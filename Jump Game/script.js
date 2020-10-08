8ab8e19bbdf16784696167ce6e9e1d1cf56588e4
O'Neal Yako <oneal.yako.55@gmail.com>
2020-10-07 23:06:12 -0400

    Added Jump Game

---
+++ Jump Game/script.js
@@ -0,0 +1,27 @@
+var character  =
+document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump() {
+    if(character.classList.contains("animate")) {
+        return
+    }
+    character.classList.add("animate");
+    setTimeout(() => {
+        character.classList.remove("animate");
+    },500);
+}
+
+var checkDead = setInterval(function() {
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+    var blockLeft =
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You LOSE!");
+    }
+}, 10);
